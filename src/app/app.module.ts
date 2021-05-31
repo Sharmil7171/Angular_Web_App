72b87143810674b2ed4288ebdfc6002a49fbc256
Sharmilkumar Patel <sharmil@sharmilmarsmbp2.home>
2021-05-30 23:34:19 -0400

    sidenav and about me design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    SidenavComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
